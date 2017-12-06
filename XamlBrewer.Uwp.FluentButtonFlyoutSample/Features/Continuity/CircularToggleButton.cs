059d566fccd27b646021fc4d5df0aa3049e689eb
Diederik Krols <diederik_krols@hotmail.com>
2017-12-06 18:27:29 +0100

    Fluent PopUp.

---
+++ XamlBrewer.Uwp.FluentButtonFlyoutSample/Features/Continuity/CircularToggleButton.cs
@@ -0,0 +1,63 @@
+﻿using Windows.UI;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Media;
+
+namespace Continuity.Controls
+{
+	public sealed class CircularToggleButton : ToggleButton
+	{
+		public object CheckedContent
+		{
+			get => GetValue(CheckedContentProperty);
+			set => SetValue(CheckedContentProperty, value);
+		}
+		public static readonly DependencyProperty CheckedContentProperty =
+			DependencyProperty.Register("CheckedContent", typeof(object), typeof(CircularToggleButton), new PropertyMetadata(null));
+
+		public DataTemplate CheckedContentTemplate
+		{
+			get => (DataTemplate)GetValue(CheckedContentTemplateProperty);
+			set => SetValue(CheckedContentTemplateProperty, value);
+		}
+		public static readonly DependencyProperty CheckedContentTemplateProperty =
+			DependencyProperty.Register("CheckedContentTemplate", typeof(DataTemplate), typeof(CircularToggleButton), new PropertyMetadata(null));
+
+		public CornerRadius CornerRadius
+		{
+			get => (CornerRadius)GetValue(CornerRadiusProperty);
+			set => SetValue(CornerRadiusProperty, value);
+		}
+		public static readonly DependencyProperty CornerRadiusProperty =
+			DependencyProperty.Register("CornerRadius", typeof(CornerRadius), typeof(CircularToggleButton), new PropertyMetadata(null));
+
+		public CornerRadius CheckedCornerRadius
+		{
+			get => (CornerRadius)GetValue(CheckedCornerRadiusProperty);
+			set => SetValue(CheckedCornerRadiusProperty, value);
+		}
+		public static readonly DependencyProperty CheckedCornerRadiusProperty =
+			DependencyProperty.Register("CheckedCornerRadius", typeof(CornerRadius), typeof(CircularToggleButton), new PropertyMetadata(null));
+
+		public Brush CheckedBackground
+		{
+			get => (Brush)GetValue(CheckedBackgroundProperty);
+			set => SetValue(CheckedBackgroundProperty, value);
+		}
+		public static readonly DependencyProperty CheckedBackgroundProperty =
+			DependencyProperty.Register("CheckedBackground", typeof(Brush), typeof(CircularToggleButton), new PropertyMetadata(null));
+
+		public Brush CheckedForeground
+		{
+			get => (Brush)GetValue(CheckedForegroundProperty);
+			set => SetValue(CheckedForegroundProperty, value);
+		}
+		public static readonly DependencyProperty CheckedForegroundProperty =
+			DependencyProperty.Register("CheckedForeground", typeof(Brush), typeof(CircularToggleButton), new PropertyMetadata(null));
+
+		public CircularToggleButton()
+		{
+			DefaultStyleKey = typeof(CircularToggleButton);
+		}
+	}
+}
--- XamlBrewer.Uwp.FluentButtonFlyoutSample/Views/MainPage.xaml.cs
+++ XamlBrewer.Uwp.FluentButtonFlyoutSample/Views/MainPage.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using Windows.UI.Xaml.Controls;
 using System;
+using System.Numerics;
+using Continuity.Extensions;
 
 namespace XamlBrewer.Uwp.FluentButtonFlyoutSample
 {
@@ -8,6 +10,17 @@ namespace XamlBrewer.Uwp.FluentButtonFlyoutSample
         public MainPage()
         {
             InitializeComponent();
+            TheGrid.EnableFluidVisibilityAnimation(centerPoint: new Vector3(0.0f, 40.0f, 0.0f), showFromScale: 0.2f, hideToScale: 0.2f, showDuration: 400, hideDuration: 400);
+        }
+
+        private void Button_Click(object sender, Windows.UI.Xaml.RoutedEventArgs e)
+        {
+            TheToggle.IsChecked = true;
+        }
+
+        private void Button_Click_1(object sender, Windows.UI.Xaml.RoutedEventArgs e)
+        {
+            TheToggle.IsChecked = false;
         }
     }
 }
